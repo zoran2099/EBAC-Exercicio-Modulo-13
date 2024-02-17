db4b9f21eb2035468bdc95a5b3a3c5b7ae09c16e
Zoran <zoran2099@gmail.com>
2024-02-17 12:04:46 -0300

    #3 update game manager to manger players

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ebac.Core.Singleton;
+using DG.Tweening;
 
 public class GameManager : Singleton<GameManager>
 {
-    
 
+
+    [Header("Player")]
+    public GameObject playerPrefab;
+    private GameObject _currentPlayer;
+
+    [Header("Enenies")]
+    public List<GameObject> enemies;
+
+    [Header("References")]
+    public Transform startPoint;
+
+    [Header("Animation")]
+    public float duration = 2f;
+    public float delay = 0.5f;
+    public Ease Ease = Ease.OutBack;
+
+
+    private void Start()
+    {
+        SpawnPlayer();
+
+    }
+
+    private void SpawnPlayer()
+    {
+
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position = startPoint.transform.position;
+        _currentPlayer.transform.DOScale(0, duration).SetEase(Ease).From();
+
+    }
 }
