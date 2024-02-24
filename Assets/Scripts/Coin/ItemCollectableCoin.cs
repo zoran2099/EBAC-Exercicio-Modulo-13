23ce4080cd7db96de13a07c5c7e7c32d26faf2fe
Zoran <zoran2099@gmail.com>
2024-02-23 21:06:33 -0300

    #29 collect coin animation

--- Assets/Scripts/Coin/ItemCollectableCoin.cs
+++ Assets/Scripts/Coin/ItemCollectableCoin.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,23 @@ public class ItemCollectableCoin : ItemCollectableBase
 {
     protected override void Collect()
     {
-        base.Collect();
         ItemManager.Instance.AddCount();
+        CollectCoin();
+
+    }
+
+    public float duration = 1f; // Duração da animação
+
+    public void CollectCoin()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Append(gameObject.transform.DORotate(new Vector3(0, 360, 0), duration, RotateMode.FastBeyond360));
+
+        // Adiciona uma animação de escala à sequência
+        sequence.Join(gameObject.transform.DOScale(Vector3.zero, duration));
+
+        // Callback para destruir o objeto quando a animação terminar
+        sequence.OnComplete(() => Destroy(gameObject));
     }
 }
