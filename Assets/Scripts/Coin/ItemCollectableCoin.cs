8f86bfe9fd8f7a9f779068dfe7efd5008641a5fd
Zoran <zoran2099@gmail.com>
2024-04-13 18:09:46 -0300

    #34 Adding VFX when collecting items

--- Assets/Scripts/Coin/ItemCollectableCoin.cs
+++ Assets/Scripts/Coin/ItemCollectableCoin.cs
@@ -6,33 +6,60 @@ using UnityEngine;
 
 public class ItemCollectableCoin : ItemCollectableBase
 {
+    [SerializeField]
+    private float _fx_time = 2f;
     private bool _isCollectable = true;
     public float duration = 1f; // Duração da animação
+    public ParticleSystem ParticleSystem;
+
+    private void Awake()
+    {
+        if (ParticleSystem != null) ParticleSystem.transform.SetParent(null);
+    }
 
     protected override void Collect()
     {
         if (!_isCollectable) return;
 
         ItemManager.Instance.AddCount();
-        CollectCoin();
-        _isCollectable=false;
-   
+
+        if (ParticleSystem != null)
+        {
+            ParticleSystem.Play();
+
+        }
+        //FXCollectCoinDotTween();
+
+        _isCollectable = false;
+        gameObject.SetActive(false);
+
+        Invoke(nameof(CleanObjects), _fx_time);
+
+    }
+
+    public void CleanObjects()
+    {
+        if(ParticleSystem != null)
+        {
+            ParticleSystem.gameObject.SetActive(false);
+            Destroy(ParticleSystem.gameObject);
+            Destroy(gameObject);
+        }
     }
 
 
-    public void CollectCoin()
-    {       
-
+    public void FXCollectCoinDotTween()
+    {        
         Sequence sequence = DOTween.Sequence();
 
         sequence.Append(gameObject.transform.DORotate(new Vector3(0, 360, 0), duration, RotateMode.FastBeyond360));
 
         // Adiciona uma animação de escala à sequência
         sequence.Join(gameObject.transform.DOScale(Vector3.zero, duration));
-
+                
         // Callback para destruir o objeto quando a animação terminar
         // aqui não consegui usar o Destroy(gameObject) por causa de mensagens de alerta do DoTween
-        sequence.OnComplete(() => gameObject.SetActive(false));
+        sequence.OnComplete(() => gameObject.SetActive(false) );
 
 
 
