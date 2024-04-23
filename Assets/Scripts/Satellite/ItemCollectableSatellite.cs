6a1d03d0c8d32ee41a1240c7b9653deec53bf060
Zoran <zoran2099@gmail.com>
2024-04-23 20:11:38 -0300

    #35 lesson 5: Adding SFX on collect satellite

--- Assets/Scripts/Satellite/ItemCollectableSatellite.cs
+++ Assets/Scripts/Satellite/ItemCollectableSatellite.cs
@@ -8,6 +8,14 @@ public class ItemCollectableSatellite : ItemCollectableBase
 
     private bool _isCollectable = true;
 
+    [Header("Sound Settings")]
+    public AudioSource AudioSource;
+
+    private void Awake()
+    {
+        if (AudioSource != null) AudioSource.transform.SetParent(null);
+    }
+
     protected override void Collect()
     {
         if (!_isCollectable) return;
@@ -15,6 +23,8 @@ public class ItemCollectableSatellite : ItemCollectableBase
         ItemManager.Instance.AddSatellite();
 
         CollectSatellite();
+
+        if (AudioSource != null) AudioSource.Play();
 
 
         _isCollectable = false;
